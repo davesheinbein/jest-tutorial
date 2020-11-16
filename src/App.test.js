3700946009fb823d6d7835e9edeb703c19b00248
David Sheinbein <davesheinbein@gmail.com>
2020-11-16 14:58:54 -0800

    first commit

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+// First test to see if first test passes
+test('fake test 1', () => {
+    expect(true).toBeTruthy();
+});
+
+// First test to see if second test fails
+test('fake test 2', () => {
+	expect(false).toBeTruthy();
+});
