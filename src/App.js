54756c428cf7a7e97505c4d3be61fcf7d61684a8
David Sheinbein <davesheinbein@gmail.com>
2020-11-16 15:20:01 -0800

    first tests

---
+++ src/App.js
@@ -0,0 +1,6 @@
+export const add = (x, y) => {
+	return x + y;
+};
+
+// automatic return statement no return included necessary
+// export const subtract = (x, y) => x - y;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
+// v to push to remote branch v
+// git add -A
+// git commit -m 'message'
+// git push remote master
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
 // First test to see if first test passes
-test('fake test 1', () => {
-    expect(true).toBeTruthy();
-});
+// test('fake test 1', () => {
+// 	expect(true).toBeTruthy();
+// });
 
 // First test to see if second test fails
-test('fake test 2', () => {
-	expect(false).toBeTruthy();
+// test('fake test 2', () => {
+// 	expect(false).toBeTruthy();
+// });
+
+// First test to see if second test passes
+// test('fake test 2', () => {
+// 	expect(false).toBeFalsy();
+// });
+
+// Testing a function
+import { add } from './App';
+// console.log(add(1, 2));
+test('add', () => {
+	// const value = add(1, 2);
+	// expect(value).toBe(3);
+	expect(add(1, 2)).toBe(3);
+	expect(add(5, 2)).toBe(7);
 });
